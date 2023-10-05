b106c7008c9dab2fb353cfd3af166f0aed92d6f4
Gerardo Arzate <gerardoarzate55@gmail.com>
2023-10-04 21:43:27 -0600

    Update frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,20 @@
-import * as THREE from 'three';
+// import * as THREE from 'three';
 
+(async () => {
+    let wsConfig;
+    try {
+        wsConfig = await fetch('ws-config');
+    } catch (e) {
+        console.log(`Couldn't get WS config: ${e.message}`);
+    }
+    
+    const ws = new WebSocket(url);
+    
+    ws.onopen = event => {
+        console.log(event)
+        ws.send('Hola');
+    }
+    
+    ws.onmessage(data => console.log(data))
+
+});
